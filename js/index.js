0babd2168b890ec9f2f666a7e2ae7ec8f7dfd91c
Aleksey <alekcyvolkov@gmail.com>
2020-03-23 04:45:27 +0300

    header add

--- js/index.js
+++ js/index.js
@@ -2,9 +2,28 @@ document.addEventListener("DOMContentLoaded", onLoad);
 
 function onLoad() {
   console.log('load')
+  languageBtnInit()
   setTimeout(loadingEnd, 3000)
 }
 
+function languageBtnInit() {
+  if (window.location.hash && window.location.hash === '#ru') {
+    document.getElementById('lang-btn-ru').prepend('* ')
+  } else {
+    document.getElementById('lang-btn-eng').prepend('* ')
+  }
+}
+
 function loadingEnd() {
-  //document.getElementById('loader').remove()
+  document.getElementById('loader-container').remove();
+  document.getElementById('input-container').style.display = 'block';
+  introType();
+}
+
+function introType() {
+  new TypeIt("#input-container", {
+    strings: ["This is a great string.", "But here is a better one."],
+    speed: 50,
+    waitUntilVisible: false
+  }).go();
 }
